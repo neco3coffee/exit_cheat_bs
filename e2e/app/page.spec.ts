e1f61d2e1b79a7b3ca863d908c78eccedef6bde4
neco3coffee <neco3coffee@gmail.com>
2025-10-02 00:40:06 +0900

    fix test

--- e2e/app/page.spec.ts
+++ e2e/app/page.spec.ts
@@ -33,14 +33,15 @@ test.describe('ユーザーがタグでプレイヤーを検索できる#1', () 
   test('ローマ字と数字以外の文字を含むタグを入力して検索して、アラートが表示されることを確認する#4', async ({ page }) => {
     // タグ入力欄にY2YPGCGC!@#を入力
     await page.getByPlaceholder('#Y2YPGCGC').fill('Y2YPGCGC!@#');
-    // Enterキーを押して検索
-    await page.getByPlaceholder('#Y2YPGCGC').press('Enter');
 
     // アラートが表示されることを確認
     page.on('dialog', async dialog => {
       expect(dialog.message()).toBe('タグは英数字で構成されている必要があります');
       await dialog.dismiss();
     });
+
+    // Enterキーを押して検索
+    await page.getByPlaceholder('#Y2YPGCGC').press('Enter');
   });
 
   // タグの長さが6文字未満のタグを入力して検索して、アラートが表示されることを確認する
@@ -60,14 +61,15 @@ test.describe('ユーザーがタグでプレイヤーを検索できる#1', () 
   test('タグの長さが10文字を超えるタグを入力して検索して、アラートが表示されることを確認する#6', async ({ page }) => {
     // タグ入力欄にY2YPGCGC123を入力
     await page.getByPlaceholder('#Y2YPGCGC').fill('Y2YPGCGC123');
-    // Enterキーを押して検索
-    await page.getByPlaceholder('#Y2YPGCGC').press('Enter');
 
     // アラートが表示されることを確認
     page.on('dialog', async dialog => {
       expect(dialog.message()).toBe('タグの長さは6〜10文字である必要があります');
       await dialog.dismiss();
     });
+
+    // Enterキーを押して検索
+    await page.getByPlaceholder('#Y2YPGCGC').press('Enter');
   });
 
 })
