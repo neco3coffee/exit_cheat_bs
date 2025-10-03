3dbf086e6fb5bf0286e51410372769ff05bfd9de
neco3coffee <neco3coffee@gmail.com>
2025-10-04 06:04:22 +0900

    fix

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
         <Header />
         <main>{children}</main>
         <Footer />
-        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />
+        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID!} />
         <Suspense fallback={null}>
           <Analytics />
         </Suspense>
