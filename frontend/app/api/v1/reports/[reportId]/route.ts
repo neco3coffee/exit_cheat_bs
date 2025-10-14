4e29be945a8bf6eef4b7faf4bf9c584b58aacc9b
neco3coffee <neco3coffee@gmail.com>
2025-10-15 00:16:04 +0900

    paramsがPromiseになってなかったので修正

--- frontend/app/api/v1/reports/[reportId]/route.ts
+++ frontend/app/api/v1/reports/[reportId]/route.ts
@@ -2,10 +2,11 @@ import { type NextRequest, NextResponse } from "next/server";
 
 export async function PUT(
   req: NextRequest,
-  { params }: { params: { reportId: string } },
+  { params }: { params: Promise<{ reportId: string }> },
 ) {
   const body = await req.json();
-  const res = await fetch(`http://app:3000/api/v1/reports/${params.reportId}`, {
+  const { reportId } = await params;
+  const res = await fetch(`http://app:3000/api/v1/reports/${reportId}`, {
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
