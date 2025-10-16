8b2780325a6faf623b009b2fcac774faabe8bc5b
neco3coffee <neco3coffee@gmail.com>
2025-10-16 13:58:39 +0900

    Footerのnavにactive style追加 pathに応じて

--- frontend/app/_components/Footer/index.tsx
+++ frontend/app/_components/Footer/index.tsx
@@ -1,16 +1,23 @@
+"use client";
 import { CircleUserRound, House } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import styles from "./index.module.scss";
 
 const Footer = () => {
+  const pathname = usePathname();
+
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
-        <Link href="/">
+        <Link href="/" className={pathname === "/" ? styles.active : ""}>
           <House />
         </Link>
-        <Link href="/ranked">
+        <Link
+          href="/ranked"
+          className={pathname === "/ranked" ? styles.active : ""}
+        >
           <Image
             src="/icon_ranked_front 1.png"
             alt="Logo"
@@ -18,7 +25,10 @@ const Footer = () => {
             height={26}
           />
         </Link>
-        <Link href="/account">
+        <Link
+          href="/account"
+          className={pathname === "/account" ? styles.active : ""}
+        >
           <CircleUserRound />
         </Link>
       </div>
