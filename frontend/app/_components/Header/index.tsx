9e46df56788647e640192242e40f70070406a4df
neco3coffee <neco3coffee@gmail.com>
2025-10-28 20:48:47 +0900

    DropDownMenuはproduction時のstreamingでhidrationとマウントで重複して表示されるので削除

--- frontend/app/_components/Header/index.tsx
+++ frontend/app/_components/Header/index.tsx
@@ -1,16 +1,22 @@
-import LocaleBox from "./_components/client/LocaleBox";
+import { cacheLife } from "next/cache";
+import { Suspense } from "react";
+import LocaleBoxContent from "./_components/client/LocaleBoxContent";
 import styles from "./index.module.scss";
 
-const Header = () => {
-  return (
-    <>
-      <header className={`${styles.header}  `}>
-        <div className={styles.logoContainer}></div>
-        <h1>SafeBrawl</h1>
-        <LocaleBox />
-      </header>
-    </>
-  );
-};
+export default async function Header() {
+  "use cache";
+  cacheLife("max");
 
-export default Header;
+  return (
+    <header className={styles.header}>
+      <div className={styles.logoContainer}></div>
+      <h1>SafeBrawl</h1>
+
+      <div className={styles.localeContainer}>
+        <Suspense fallback={null}>
+          <LocaleBoxContent />
+        </Suspense>
+      </div>
+    </header>
+  );
+}
