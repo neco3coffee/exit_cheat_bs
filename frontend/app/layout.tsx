58334df65e10b20477fcccecb939dfe07016b1ea
neco3coffee <neco3coffee@gmail.com>
2025-11-02 01:27:31 +0900

    fix

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,3 +1,15 @@
+import Script from "next/script";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+  return (
+    <>
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3651729056445822"
+        crossOrigin="anonymous"
+        strategy="afterInteractive"
+      />
+      {children}
+    </>
+  );
 }
