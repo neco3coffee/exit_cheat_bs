5c1e74448c1885c3277ce6965b77c2f51bd89e0b
neco3coffee <neco3coffee@gmail.com>
2025-11-01 21:07:53 +0900

    fix build

--- frontend/app/_components/Header/index.tsx
+++ frontend/app/_components/Header/index.tsx
@@ -1,10 +1,10 @@
 import { MailPlus } from "lucide-react";
 import { cacheLife } from "next/cache";
 import { Suspense } from "react";
+import ServerLocaleMessageProviderWrapper from "@/app/_messages/ServerLocaleMessageProviderWrapper";
 import LocaleBoxContent from "./_components/client/LocaleBoxContent";
 import SafeBrawlMenuContent from "./_components/client/SafeBrawlMenuContent";
 import styles from "./index.module.scss";
-import ServerLocaleMessageProviderWrapper from "@/app/_messages/ServerLocaleMessageProviderWrapper";
 
 export default async function Header({
   params,
@@ -27,9 +27,9 @@ export default async function Header({
         </a>
       </div>
       <Suspense fallback={<h1>SafeBrawl</h1>}>
-      <ServerLocaleMessageProviderWrapper params={params}>
-        <SafeBrawlMenuContent />
-      </ServerLocaleMessageProviderWrapper>
+        <ServerLocaleMessageProviderWrapper params={params}>
+          <SafeBrawlMenuContent />
+        </ServerLocaleMessageProviderWrapper>
       </Suspense>
 
       <div className={styles.localeContainer}>
