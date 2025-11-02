eb2afcf2ff6cd68c531fc72716b1bc86ce91ca6f
neco3coffee <neco3coffee@gmail.com>
2025-11-02 22:18:35 +0900

    api cache無効化

--- backend/app/controllers/api/v1/stats_controller.rb
+++ backend/app/controllers/api/v1/stats_controller.rb
@@ -3,13 +3,11 @@ module Api
     class StatsController < ApplicationController
       # GET /api/v1/stats
       def index
-        # Cache the stats for 1 hour to reduce database load
-        stats = Rails.cache.fetch('landing_page_stats', expires_in: 1.hour) do
+        stats =
           {
             totalReportsCount: Report.count,
             totalPlayersCount: Player.count
           }
-        end
 
         render json: stats, status: :ok
       end
