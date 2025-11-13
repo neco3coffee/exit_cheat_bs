4305762c14d0d975a3962d706e2908b68d4db79f
neco3coffee <neco3coffee@gmail.com>
2025-11-13 15:46:42 +0900

    npm run lint

--- frontend/app/_components/AdsenseWrapper/index.tsx
+++ frontend/app/_components/AdsenseWrapper/index.tsx
@@ -3,6 +3,16 @@ import dynamic from "next/dynamic";
 
 const Adsense = dynamic(() => import("../Adsense"), { ssr: false });
 
-export default function AdsenseWrapper() {
-  return <Adsense />;
+export default function AdsenseWrapper({ maxHeight }: { maxHeight?: number }) {
+  return (
+    <div
+      style={{
+        width: "100%",
+        maxHeight: maxHeight ? `${maxHeight}px` : "none",
+        overflow: "hidden",
+      }}
+    >
+      <Adsense />
+    </div>
+  );
 }
