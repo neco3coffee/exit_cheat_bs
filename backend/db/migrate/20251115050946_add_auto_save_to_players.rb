fe5cde383a112bdf37a9329dd6a6812107ba1fd1
neco3coffee <neco3coffee@gmail.com>
2025-11-15 14:44:45 +0900

    robocop

--- backend/db/migrate/20251115050946_add_auto_save_to_players.rb
+++ backend/db/migrate/20251115050946_add_auto_save_to_players.rb
@@ -2,6 +2,6 @@ class AddAutoSaveToPlayers < ActiveRecord::Migration[8.0]
   def change
     add_column :players, :auto_save_enabled, :boolean
     add_column :players, :auto_save_expires_at, :datetime
-    add_index :players, [:auto_save_enabled, :auto_save_expires_at]
+    add_index :players, [ :auto_save_enabled, :auto_save_expires_at ]
   end
 end
