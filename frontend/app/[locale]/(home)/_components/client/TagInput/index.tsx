1407cf53aa17230de15bbf40938747a02290dccb
neco3coffee <neco3coffee@gmail.com>
2025-11-16 02:49:13 +0900

    fixxx

--- frontend/app/[locale]/(home)/_components/client/TagInput/index.tsx
+++ frontend/app/[locale]/(home)/_components/client/TagInput/index.tsx
@@ -113,7 +113,7 @@ export default function TagInput() {
                 <DropdownMenuItem
                   key={item.tag}
                   onSelect={(event) => {
-                    window.location.replace(`/players/${item.tag}`);
+                    router.push(`/players/${item.tag}`);
                   }}
                   className={styles.dropdownMenuItem}
                 >
